f1b0fd23df8e92ae210b6b39266461a4efdd51a4
Kevin Hester <kevinh@geeksville.com>
2013-02-02 08:35:39 -0800

    preference goo

--- src/main/scala/com/ridemission/scandroid/UsesPreferences.scala
+++ src/main/scala/com/ridemission/scandroid/UsesPreferences.scala
@@ -4,16 +4,22 @@ import android.content.Context
 import android.preference.PreferenceManager
 import android.content.SharedPreferences.OnSharedPreferenceChangeListener
 import android.content.SharedPreferences
+import scala.Enumeration
 
 /**
  * A mixin for easier preference access
  */
-trait UsesPreferences { context: Context =>
-  lazy val preferences = PreferenceManager.getDefaultSharedPreferences(this)
+trait UsesPreferences {
+
+  def context: Context
+
+  lazy val preferences = PreferenceManager.getDefaultSharedPreferences(context)
 
   def stringPreference(s: String, defVal: String = "") = tryOrElse(preferences.getString(s, defVal), defVal)
   def intPreference(s: String, defVal: Int) = tryOrElse(preferences.getString(s, defVal.toString).toInt, defVal)
   def boolPreference(s: String, defVal: Boolean) = tryOrElse(preferences.getBoolean(s, defVal), defVal)
+  def floatPreference(s: String, defVal: Float) = tryOrElse(preferences.getFloat(s, defVal), defVal)
+  def enumPreference[T <: Enumeration](s: String, enum: T, defVal: T#Value) = tryOrElse(enum.withName(preferences.getString(s, defVal.toString)), defVal)
 
   def tryOrElse[T](f: => T, elseVal: T) = try {
     f
