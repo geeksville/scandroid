b50d07bec9851e1aaa9132fd993bdea8518fc2d1
Kevin Hester <kevinh@geeksville.com>
2013-02-06 18:14:56 -0800

    Add ScalaPagerAdapter

---
+++ src/main/scala/com/ridemission/scandroid/PagerPage.scala
@@ -0,0 +1,18 @@
+package com.ridemission.scandroid
+
+/**
+ * A fragment that is a page on a paged view
+ * FIXME - move to scandroid utilities
+ */
+trait PagerPage extends AndroidLogger {
+  var isShown = false
+
+  def onPageShown() {
+    isShown = true
+    debug("Page shown: " + this)
+  }
+  def onPageHidden() {
+    isShown = false
+    debug("Page hidden: " + this)
+  }
+}
---
+++ src/main/scala/com/ridemission/scandroid/ScalaPagerAdapter.scala
@@ -0,0 +1,66 @@
+package com.ridemission.scandroid
+
+import _root_.android.os.Bundle
+import android.content.Intent
+import android.widget._
+import android.content.ServiceConnection
+import android.content.ComponentName
+import android.os.IBinder
+import android.content.Context
+import android.content.res.Configuration
+import android.os.Handler
+import scala.language.postfixOps
+import android.hardware.usb.UsbManager
+import android.content.BroadcastReceiver
+import android.content.IntentFilter
+import android.view.View
+import android.view.Menu
+import android.widget.AdapterView.OnItemSelectedListener
+import android.view.MenuItem
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentManager
+import android.support.v4.app.FragmentPagerAdapter
+import android.support.v4.app.FragmentActivity
+import android.support.v4.view.ViewPager
+import android.support.v4.app.FragmentStatePagerAdapter
+import android.view.ViewGroup
+
+case class PageInfo(title: String, generator: () => Fragment)
+
+/**
+ * FIXME
+ * We might want to use a version of the pager adapter that destroys fragments when not in use (so we don't spend cycles updating RC channels when not visible)
+ *
+ */
+class ScalaPagerAdapter(fm: FragmentManager, val pages: IndexedSeq[PageInfo]) extends FragmentPagerAdapter(fm) {
+
+  private var curPage: Option[PagerPage] = None
+
+  override def getItem(position: Int) = {
+    // getItem is called to instantiate the fragment for the given page.
+    // Return a DummySectionFragment (defined as a static inner class
+    // below) with the page number as its lone argument.
+    val fragment = pages(position).generator()
+    //Bundle args = new Bundle();
+    //args.putInt(DummySectionFragment.ARG_SECTION_NUMBER, position + 1);
+    //fragment.setArguments(args);
+    fragment
+  }
+
+  override def getCount() = pages.size
+
+  override def getPageTitle(i: Int) = pages(i).title
+
+  override def setPrimaryItem(container: ViewGroup, position: Int, obj: Object) {
+    super.setPrimaryItem(container, position, obj)
+
+    // If the fragment doesn't care to be notified of extra page stuff - don't bother with it
+    val newPage = if (obj.isInstanceOf[PagerPage]) Some(obj.asInstanceOf[PagerPage]) else None
+
+    if (curPage != newPage) { // Android seems to send redundant notifications - don't get confused
+      curPage.foreach(_.onPageHidden())
+      curPage = newPage
+      newPage.foreach(_.onPageShown())
+    }
+  }
+}
