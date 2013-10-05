243b4fdb75b10ba8da2461379d466527ba885f82
Kevin Hester <kevinh@geeksville.com>
2013-10-05 07:47:23 -1000

    new util funct

--- src/main/scala/com/ridemission/scandroid/AlertUtil.scala
+++ src/main/scala/com/ridemission/scandroid/AlertUtil.scala
@@ -8,6 +8,39 @@ import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.content.DialogInterface
 import android.widget.Button
+import android.support.v4.app.FragmentActivity
+
+class SimpleOkayDialog(prompt: String, onOk: () => Unit, icon: Int = -1) extends DialogFragment {
+  override def onCreateDialog(savedInstanceState: Bundle) = {
+    val builder = new AlertDialog.Builder(getActivity)
+
+    // 2. Chain together various setter methods to set the dialog characteristics
+    if (icon != -1)
+      builder.setIcon(icon)
+    builder.setMessage(prompt)
+    builder.setPositiveButton("OK", onOk)
+    //   .setTitle(R.string.dialog_title);
+
+    // 3. Get the AlertDialog from create()
+    builder.create()
+  }
+
+  override def onDismiss(i: DialogInterface) {
+    super.onDismiss(i)
+    onOk()
+  }
+}
+
+object SimpleOkayDialog {
+  /**
+   * Ask OK or cancel for a question...
+   */
+  def show(activity: FragmentActivity, prompt: String, icon: Int = -1, onOk: () => Unit = { () => }, name: String = "okay") {
+    val newFragment = new SimpleOkayDialog(prompt, onOk, icon)
+
+    newFragment.show(activity.getSupportFragmentManager(), name)
+  }
+}
 
 class SimpleYesNoDialog(prompt: String, onOk: () => Unit, onCancel: () => Unit) extends DialogFragment {
   override def onCreateDialog(savedInstanceState: Bundle) = {
